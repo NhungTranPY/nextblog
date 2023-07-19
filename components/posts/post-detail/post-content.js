0753ab2730a05166d1ffe6dac9ab28524312e390
NhungTranPY <nhungtranpy91@gmail.com>
2023-07-19 16:36:06 +0700

    Rendering Images With The "Next Image" ( Markdown)

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,6 +1,7 @@
 import ReactMarkdown from 'react-markdown'
 import PostHeader from "./post-header"
 import classes from './post-content.module.css'
+import Image from 'next/image'
 
 
 function PostContent(props) {
@@ -9,11 +10,46 @@ function PostContent(props) {
 
     const imagePath = `/images/posts/${post.slug}/${post.image}`
 
+    const customRenderers = {
+        // image(image) {
+        //     return (
+        //         <Image 
+        //             src={`/images/posts/${post.slug}/${image.src}`} 
+        //             alt={image.alt} 
+        //             width={600} 
+        //             height={300} 
+        //         />
+        //     )
+        // },
+        paragraph(paragraph) {
+            const { node } = paragraph
+
+            if (node.children[0].type === 'image') {
+                const image = node.children[0]
+
+                return (
+                    <div className={classes.image}>
+                        <Image 
+                            src={`/images/posts/${post.slug}/${image.url}`} 
+                            alt={image.alt} 
+                            width={600} 
+                            height={300} 
+                        />
+                    </div>
+                )
+            }
+
+            return (
+                <p>{paragraph.children}</p>
+            )
+        }
+    }
+
     return (
         <>
         <article className={classes.content}>
             <PostHeader title={post.title} image={imagePath} />
-            <ReactMarkdown>{post.content}</ReactMarkdown>
+            <ReactMarkdown renderers={customRenderers}>{post.content}</ReactMarkdown>
         </article>
         </>
     )
